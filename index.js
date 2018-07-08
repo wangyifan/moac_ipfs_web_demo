b63e9fa61fc6712e25335b035d82b6d6561e4ad5
Yifan Wang <heavenstar@gmail.com>
2018-07-08 09:28:12 +0000

    asyn await for ipfs add

--- index.js
+++ index.js
@@ -2,27 +2,36 @@ const os = require('os');
 const util = require('util');
 const express = require("express");
 const multer  = require('multer')
-const fs = require('fs');
+const fs = require("fs")
 const ipfsAPI = require('ipfs-api')
+const redis = require('redis')
 
-const app = express()
-const userUpload = multer({ dest: os.tmpdir(), preservePath: true })
-const ipfs = ipfsAPI('localhost', '5001', {protocol: 'http'})
+const app = express();
+const userUpload = multer({ dest: os.tmpdir(), preservePath: true });
+const ipfs = ipfsAPI('localhost', '5001', {protocol: 'http'});
+const redisClient = redis.createClient();
+const ipfsFileMappingHash = 'ipfs_file_mapping_hash'
 
+// add index route
 app.get('/', (req, res) => res.send('moac ipfs web demo'))
 
-app.post('/upload', userUpload.single('add_to_ipfs'), (req, res) => {
+// add upload file route
+app.post('/upload', userUpload.single('add_to_ipfs'), async (req, res) => {
     let tmpFilePath = util.format('%s/%s', os.tmpdir(), req.file.filename)
     console.log("Uploaded to", tmpFilePath)
     const files = [{
         path: req.file.filename,
         content: fs.createReadStream(tmpFilePath)
-    }]
-    ipfs.files.add(files, (err, files) => {
-        console.log(files);
-        fs.unlinkSync(tmpFilePath);
-    })
-    res.send({message: 'success'});
+    }];
+    const addFileResult = await ipfs.files.add(files)
+    const fileHash = addFileResult[0]['hash']
+    console.log("ipfs added:", fileHash)
+    // remove tmp file
+    fs.unlinkSync(tmpFilePath);
+    // register with redis
+    redisClient.hset(ipfsFileMappingHash, req.file.originalname, fileHash)
+    res.send({message: 'success', hash: fileHash, name: req.file.originalname});
 })
 
+// start server
 app.listen(18373, () => console.log('Ready to serve!'))
